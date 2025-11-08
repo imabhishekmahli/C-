f7b2fb040d18f6b3c2ee743dc70079a0c7807a86
imabhishekmahli <abhi8h3k@gmail.com>
2025-11-08 17:29:51 +0530

    divisible by 5 or not

---
+++ 01_02_divisibleBy5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  if(n%5==0)
+  {
+    cout<<"divisible by 5"<<endl;
+  }
+  else{
+      cout<<"not divisible by 5"<<endl;
+  }
+  return 0;
+}
